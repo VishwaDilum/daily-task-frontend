23fae2ca0a53792bf4ffa9e54a83406aa3bc29c4
Vishwa Dilum <138286049+VishwaDilum@users.noreply.github.com>
2024-06-07 18:03:38 +0530

    Add task UI Integrate

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react'; // Import React if you're using JSX
 import './App.css';
 import Side from "../src/components/left-side"; // Assuming Side is the default export from left-side
-
+import {} from 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
-  function name(username: string) {
-    return username + "ICET";
-  }
-
-  let username: string = "Dilum"; // Ensure consistent typing
-
   return (
-    <div>
-      <h1>Hi {name(username)} </h1>
-      <Side title ="Bread" description ="Good Bread"></Side>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-3'><Side></Side></div>
+        <div className='col-3'></div>
+        <div className='col-3'></div>
+        <div className='col-3'></div>
+      </div>
     </div>
   );
 }
