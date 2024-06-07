63e72b98a3969b2a0e6bb5819c6c42bb090548dc
Vishwa Dilum <138286049+VishwaDilum@users.noreply.github.com>
2024-06-08 00:00:18 +0530

    routing config

--- src/components/forgotpass.tsx
+++ src/components/forgotpass.tsx
@@ -1,40 +1,78 @@
 import * as React from 'react';
+import { } from '../components/forgotpass.css'
+import homeIcon from '../assets/home.png'
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 function forgotpassword() {
-
-    let test : number = 5;
-
+    const history = useNavigate();
+    function test() {
+        history('/signin');
+    }
     return (
-        <div className="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabIndex = {test} role="dialog" id="modalSignin">
-  <div className="modal-dialog" role="document">
-    <div className="modal-content rounded-4 shadow">
-      <div className="modal-header p-5 pb-4 border-bottom-0">
-        <h1 className="fw-bold mb-0 fs-2">Lost The Password</h1>
-      </div>
 
-      <div className="modal-body p-5 pt-0">
-        <form className="">
-          <div className="form-floating mb-3">
-            <input type="email" className="form-control rounded-3" id="floatingInput" placeholder="name@example.com"></input>
-            <label htmlFor="floatingInput">Email address</label>
-          </div>
-          <div className="form-floating mb-3">
-            <input type="password" className="form-control rounded-3" id="floatingPassword" placeholder="Password">
+        <div className="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabIndex={2} role="dialog" id="modalSignin">
+            <button className='test' onClick={test} >
+                <div>
+                    <img src={homeIcon}></img>
+                </div>
+            </button>
+            <div className="modal-dialog" role="document">
+                <div className="modal-content rounded-4 shadow">
+                    <div className="modal-header p-5 pb-4 border-bottom-0">
+                        <h1 className="fw-bold mb-0 fs-">Lost The Password</h1>
+                    </div>
 
-            </input>
-            <label htmlFor="floatingPassword">Password</label>
-          </div>
-          <div className="form-floating mb-3">
-            <input type="password" className="form-control rounded-3" id="floatingPassword" placeholder="Password"></input>
-            <label htmlFor="floatingPassword">Re- Password</label>
-          </div>
-          <button className="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Sign up</button>
-          <small className="text-body-secondary">By clicking Sign up, you agree to the terms of use.</small>
-          <hr className="my-4"></hr>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
+                    <div className="modal-body p-5 pt-0">
+                        <form className="">
+                            <div className="form-floating mb-3">
+                                <input type="email" className="form-control rounded-3" id="floatingInput" placeholder="name@example.com"></input>
+                                <label htmlFor="floatingInput">Email address</label>
+                            </div>
+
+                            <div className='row'>
+                                <div className='col-4'>
+                                    <button type="button" className="btn btn-secondary">  Send  OTP</button>
+                                </div>
+                                <div className='col-4'>
+                                    <input type="email" className="form-control rounded-3" id="floatingInput" placeholder="Enter OTP"></input>
+                                </div>
+                                <div className='col-4'>
+
+                                </div>
+                            </div>
+                            <div className='row'>
+                                <div className='col-4'>
+
+                                </div>
+                                <div className='col-4'>
+
+                                </div>
+                                <div className='col-4' id='confirmBtn'>
+                                    <button type="button" className="btn btn-primary">Confirm</button>
+                                </div>
+                            </div>
+
+
+                            <div className="form-floating mb-3">
+                                <input type="email" className="form-control rounded-3" id="floatingInput" placeholder="name@example.com" disabled></input>
+                                <label htmlFor="floatingInput">Enter Password</label>
+                            </div>
+
+                            <div className="form-floating mb-3">
+                                <input type="email" className="form-control rounded-3" id="floatingInput" placeholder="name@example.com" disabled></input>
+                                <label htmlFor="floatingInput">Re-Enter</label>
+                            </div>
+
+                            <div className='col-5'>
+                                <button type="button" className="btn btn-outline-primary" disabled>Reset Password</button>
+                            </div>
+
+                            <hr className="my-4"></hr>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
